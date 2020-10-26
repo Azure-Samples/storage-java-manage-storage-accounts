f5933b51e19f990bcece5d429dfdec708b11a664
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:15:35 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/storage/samples/ManageStorageAccount.java
+++ src/main/java/com/azure/resourcemanager/storage/samples/ManageStorageAccount.java
@@ -1,46 +1,49 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.storage.samples;
+package com.azure.resourcemanager.storage.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.azure.management.storage.StorageAccount;
-import com.microsoft.azure.management.storage.StorageAccountEncryptionStatus;
-import com.microsoft.azure.management.storage.StorageAccountKey;
-import com.microsoft.azure.management.storage.StorageAccounts;
-import com.microsoft.azure.management.storage.StorageService;
-import com.microsoft.rest.LogLevel;
 
-import java.io.File;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.resourcemanager.storage.models.StorageAccount;
+import com.azure.resourcemanager.storage.models.StorageAccountEncryptionStatus;
+import com.azure.resourcemanager.storage.models.StorageAccountKey;
+import com.azure.resourcemanager.storage.models.StorageAccounts;
+import com.azure.resourcemanager.storage.models.StorageService;
+
 import java.util.List;
 import java.util.Map;
 
 /**
  * Azure Storage sample for managing storage accounts -
- *  - Create a storage account
- *  - Get | regenerate storage account access keys
- *  - Create another storage account
- *  - Create another storage account of V2 kind
- *  - List storage accounts
- *  - Delete a storage account.
+ * - Create a storage account
+ * - Get | regenerate storage account access keys
+ * - Create another storage account
+ * - Create another storage account of V2 kind
+ * - List storage accounts
+ * - Delete a storage account.
  */
 
 public final class ManageStorageAccount {
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     *
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
-        final String storageAccountName = Utils.createRandomName("sa");
-        final String storageAccountName2 = Utils.createRandomName("sa2");
-        final String storageAccountName3 = Utils.createRandomName("sa3");
-        final String rgName = Utils.createRandomName("rgSTMS");
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
+        final String storageAccountName = Utils.randomResourceName(azureResourceManager, "sa", 8);
+        final String storageAccountName2 = Utils.randomResourceName(azureResourceManager, "sa2", 8);
+        final String storageAccountName3 = Utils.randomResourceName(azureResourceManager, "sa3", 8);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgSTMS", 8);
         try {
 
             // ============================================================
@@ -48,7 +51,7 @@ public final class ManageStorageAccount {
 
             System.out.println("Creating a Storage Account");
 
-            StorageAccount storageAccount = azure.storageAccounts().define(storageAccountName)
+            StorageAccount storageAccount = azureResourceManager.storageAccounts().define(storageAccountName)
                     .withRegion(Region.US_EAST)
                     .withNewResourceGroup(rgName)
                     .create();
@@ -78,7 +81,7 @@ public final class ManageStorageAccount {
 
             System.out.println("Creating a 2nd Storage Account");
 
-            StorageAccount storageAccount2 = azure.storageAccounts().define(storageAccountName2)
+            StorageAccount storageAccount2 = azureResourceManager.storageAccounts().define(storageAccountName2)
                     .withRegion(Region.US_EAST)
                     .withNewResourceGroup(rgName)
                     .create();
@@ -105,11 +108,11 @@ public final class ManageStorageAccount {
 
             System.out.println("Creating a V2 Storage Account");
 
-            StorageAccount storageAccount3 = azure.storageAccounts().define(storageAccountName3)
-                    .withRegion(Region.US_EAST)
-                    .withNewResourceGroup(rgName)
-                    .withGeneralPurposeAccountKindV2()
-                    .create();
+            azureResourceManager.storageAccounts().define(storageAccountName3)
+                .withRegion(Region.US_EAST)
+                .withNewResourceGroup(rgName)
+                .withGeneralPurposeAccountKindV2()
+                .create();
 
             System.out.println("Created V2 Storage Account");
 
@@ -118,13 +121,11 @@ public final class ManageStorageAccount {
 
             System.out.println("Listing storage accounts");
 
-            StorageAccounts storageAccounts = azure.storageAccounts();
+            StorageAccounts storageAccounts = azureResourceManager.storageAccounts();
 
-            List<StorageAccount> accounts = storageAccounts.listByResourceGroup(rgName);
-            StorageAccount sa;
-            for (int i = 0; i < accounts.size(); i++) {
-                sa = (StorageAccount) accounts.get(i);
-                System.out.println("Storage Account (" + i + ") " + sa.name()
+            PagedIterable<StorageAccount> accounts = storageAccounts.listByResourceGroup(rgName);
+            for (StorageAccount sa : accounts) {
+                System.out.println("Storage Account " + sa.name()
                         + " created @ " + sa.creationTime());
             }
 
@@ -134,43 +135,43 @@ public final class ManageStorageAccount {
             System.out.println("Deleting a storage account - " + storageAccount.name()
                     + " created @ " + storageAccount.creationTime());
 
-            azure.storageAccounts().deleteById(storageAccount.id());
+            azureResourceManager.storageAccounts().deleteById(storageAccount.id());
 
             System.out.println("Deleted storage account");
             return true;
-        } catch (Exception f) {
-            System.out.println(f.getMessage());
-            f.printStackTrace();
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().deleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
             }
         }
-        return false;
     }
 
     /**
      * Main entry point.
+     *
      * @param args the parameters
      */
     public static void main(String[] args) {
         try {
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.BODY)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
